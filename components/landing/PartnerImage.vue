3db529537f5f77b7f64e8fcceb21f6a45fb58a4b
Mehrdad GH <mehrdadghayour7@gmail.com>
2023-08-09 23:14:45 +0330

    This is a new commit for what I originally planned to be amended

---
+++ components/landing/PartnerImage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mx-10 max-sm:mx-0">
+      <img :src="require(`~/assets/img/partner/${img}`)" class=" lg:w-32" alt="" />
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
